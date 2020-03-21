6529396ed21c95ada1cde463cbe42cf4aa05f408
IkBenDeSjaak <58164424+IkBenDeSjaak@users.noreply.github.com>
2020-03-21 12:29:50 +0100

    Added Player, FloorTile and main class + some media
    player stands on tiles and is able to move left and right

---
+++ fruit-catcher/src/main/java/fruitcatcher/FruitCatcher.java
@@ -0,0 +1,82 @@
+package fruitcatcher;
+
+import fruitcatcher.tiles.FloorTile;
+import nl.han.ica.oopg.engine.GameEngine;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.sound.Sound;
+import nl.han.ica.oopg.tile.TileMap;
+import nl.han.ica.oopg.tile.TileType;
+import nl.han.ica.oopg.view.View;
+import fruitcatcher.Player;
+
+public class FruitCatcher extends GameEngine {
+    
+	private Player player;
+	
+    // Deze regel maakt het makkelijker om te refereren naar je plaatjes.
+    public static String MEDIA_URL = "src/main/java/fruitcatcher/media/";
+    
+    public static void main(String[] args) {
+        FruitCatcher fruitCatcher = new FruitCatcher();
+        fruitCatcher.runSketch();
+    }
+
+    @Override
+    public void setupGame() {
+        int worldWidth = 800;
+        int worldHeight = 600;
+        
+        // uiteraard kan je het toevoegen van
+        // nieuwe game objects misschien het beste
+        // in een aparte methode doen
+        // i.p.v. de update zo groot te maken.
+        player = new Player(this);
+        addGameObject(player, 200, 200);
+        
+        //spawn = new DiscSpawner(this, 1);
+        
+        View view = new View(worldWidth, worldHeight);
+
+        setView(view);
+        size(worldWidth, worldHeight);
+        //view.setBackground(loadImage(FruitCatcher.MEDIA_URL.concat("background.png")));
+        initializeTileMap();
+        //initializeSound();
+    }
+
+    @Override
+    public void update() {
+        // Dit doet nog helemaal niks
+        
+    }
+    
+    private void initializeTileMap() {
+        // Load Sprites
+        Sprite floorSprite = new Sprite(FruitCatcher.MEDIA_URL.concat("grassMid.png"));
+        // Create tile types with the right Tile class and sprite
+        TileType<FloorTile> floorTileType = new TileType<>(FloorTile.class, floorSprite);
+        @SuppressWarnings("rawtypes")
+        TileType[] tileTypes = {floorTileType};
+        int tileSize = 64;
+        int tilesMap[][] = {
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1},          
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,},
+        };
+        tileMap = new TileMap(tileSize, tileTypes, tilesMap);
+    }
+    
+   // private void initializeSound() {
+   // 	backgroundMusic = new Sound(this, MEDIA_URL.concat("jump_08.mp3"));
+	//	backgroundMusic.loop(-1);
+	//}
+    
+
+}
---
+++ fruit-catcher/src/main/java/fruitcatcher/Player.java
@@ -0,0 +1,76 @@
+package fruitcatcher;
+
+import java.util.List;
+
+import fruitcatcher.tiles.FloorTile;
+import nl.han.ica.oopg.collision.CollidedTile;
+import nl.han.ica.oopg.collision.CollisionSide;
+import nl.han.ica.oopg.collision.ICollidableWithTiles;
+import nl.han.ica.oopg.exceptions.TileNotFoundException;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.SpriteObject;
+import processing.core.PVector;
+
+public class Player extends SpriteObject implements ICollidableWithTiles {
+
+	private FruitCatcher fruitCatcher;
+
+	private float gravity;
+	final int size = 96;
+
+	public Player(FruitCatcher fruitCatcher) {
+		// Met `.concat()` plak je 2 strings aan elkaar.
+		// De methode returned een nieuwe String terug.
+		super(new Sprite(FruitCatcher.MEDIA_URL.concat("playerstanding.png")));
+		this.fruitCatcher = fruitCatcher;
+		gravity = 0.2f;
+		setGravity(gravity);
+	}
+
+	@Override
+	public void update() {
+		if (getX() <= 0) {
+			setxSpeed(0);
+			setX(0);
+		}
+
+		if (getX() >= fruitCatcher.width - size) {
+			setxSpeed(0);
+			setX(fruitCatcher.width - size);
+		}
+
+	}
+
+	@Override
+	public void keyPressed(int keyCode, char key) {
+		final int speed = 5;
+		if (keyCode == fruitCatcher.LEFT) {
+			setDirectionSpeed(270, speed);
+		}
+		if (keyCode == fruitCatcher.RIGHT) {
+			setDirectionSpeed(90, speed);
+		}
+
+	}
+
+	@Override
+	public void tileCollisionOccurred(List<CollidedTile> collidedTiles) {
+
+		PVector vector;
+		for (CollidedTile ct : collidedTiles) {
+			if (ct.getTile() instanceof FloorTile) {
+				if (ct.getCollisionSide() == CollisionSide.TOP) {
+					try {
+						vector = fruitCatcher.getTileMap().getTilePixelLocation(ct.getTile());
+						setY(vector.y - getHeight());
+					} catch (TileNotFoundException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+		}
+
+	}
+
+}
