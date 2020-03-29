7820d5f19b310fd688575936483f4e9d7eef7562
IkBenDeSjaak <58164424+IkBenDeSjaak@users.noreply.github.com>
2020-03-29 15:10:26 +0200

    Update fruit.java
    - removed random from Fruit class

--- fruit-catcher/src/main/java/fruitcatcher/Fruit.java
+++ fruit-catcher/src/main/java/fruitcatcher/Fruit.java
@@ -1,19 +1,15 @@
 package fruitcatcher;
 
 import java.util.List;
-import java.util.Random;
 
 import nl.han.ica.oopg.objects.GameObject;
 
 public class Fruit extends FallingObject {
 	
-	private Random random;
-
 	public Fruit(FruitCatcher fruitCatcher, String fruitObject) {
 		// Use `.concat ()` to string 2 strings together.
 		// The method returned a new String.
 		super(fruitCatcher, fruitObject);
-		random = new Random();
 	}
 
 	// @Override
