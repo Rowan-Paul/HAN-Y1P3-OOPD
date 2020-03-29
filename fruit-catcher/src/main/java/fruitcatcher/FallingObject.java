596c2f0fff02a36f642a3d75df4e65358b3d9d49
IkBenDeSjaak <58164424+IkBenDeSjaak@users.noreply.github.com>
2020-03-29 11:39:39 +0200

    Implemented FallingObject class
    + added new class, fruit extends with FallingObject
    - removed unnecessary imports

---
+++ fruit-catcher/src/main/java/fruitcatcher/FallingObject.java
@@ -0,0 +1,29 @@
+package fruitcatcher;
+
+import nl.han.ica.oopg.collision.ICollidableWithGameObjects;
+import nl.han.ica.oopg.objects.Sprite;
+import nl.han.ica.oopg.objects.SpriteObject;
+
+public abstract class FallingObject extends SpriteObject implements ICollidableWithGameObjects {
+
+	protected FruitCatcher fruitCatcher;
+
+	private int speed;
+
+	public FallingObject(FruitCatcher fruitCatcher) {
+		super(new Sprite(FruitCatcher.MEDIA_URL.concat("apple.png")));
+		this.fruitCatcher = fruitCatcher;
+		this.speed = 1;
+	}
+	
+	@Override
+	public void update() {
+		if (this.y > 0) {
+			setDirectionSpeed(180, speed);
+		}   
+		if (this.y > fruitCatcher.getHeight()) {
+			fruitCatcher.deleteGameObject(this);
+		}
+	}
+
+}
--- fruit-catcher/src/main/java/fruitcatcher/Fruit.java
+++ fruit-catcher/src/main/java/fruitcatcher/Fruit.java
@@ -2,41 +2,22 @@ package fruitcatcher;
 
 import java.util.List;
 
-import nl.han.ica.oopg.collision.ICollidableWithGameObjects;
 import nl.han.ica.oopg.objects.GameObject;
-import nl.han.ica.oopg.objects.Sprite;
-import nl.han.ica.oopg.objects.SpriteObject;
 
-public class Fruit extends SpriteObject implements ICollidableWithGameObjects {
-
-	private FruitCatcher fruitCatcher;
-	
-	private int speed;
+public class Fruit extends FallingObject {
 
 	public Fruit(FruitCatcher fruitCatcher) {
 		// Use `.concat ()` to string 2 strings together.
 		// The method returned a new String.
-		super(new Sprite(FruitCatcher.MEDIA_URL.concat("apple.png")));
-		this.fruitCatcher = fruitCatcher;
-		this.speed = 1;
+		super(fruitCatcher);
 	}
 
-	@Override
-	public void update() {
-		if (this.y > 0) {
-			setDirectionSpeed(180, speed);
-		}   
-		if (this.y > fruitCatcher.getHeight()) {
-			fruitCatcher.deleteGameObject(this);
+	// @Override
+	public void gameObjectCollisionOccurred(List<GameObject> collidedGameObjects) {
+		for (GameObject go : collidedGameObjects) {
+			if (go instanceof Player) {
+				fruitCatcher.deleteGameObject(this);
+			}
 		}
 	}
-	
-	//@Override
-	public void gameObjectCollisionOccurred(List<GameObject>collidedGameObjects) { 
-		 for (GameObject go: collidedGameObjects) { 
-			 if (go instanceof Player) { 
-				 fruitCatcher.deleteGameObject(this);
-			 } 
-		 } 
-	 }
 }
