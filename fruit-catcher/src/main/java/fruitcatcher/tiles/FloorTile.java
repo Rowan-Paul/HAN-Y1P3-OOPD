00e694380569d5417bfeef5a01789e7dde05d24f
Rowan-Paul <46132597+Rowan-Paul@users.noreply.github.com>
2020-03-31 17:39:55 +0200

    Added Floortile Javadoc
    Added:
     - Floortile Javadoc
     - Generated new Javadoc

--- fruit-catcher/src/main/java/fruitcatcher/tiles/FloorTile.java
+++ fruit-catcher/src/main/java/fruitcatcher/tiles/FloorTile.java
@@ -3,9 +3,14 @@ package fruitcatcher.tiles;
 import nl.han.ica.oopg.objects.Sprite;
 import nl.han.ica.oopg.tile.Tile;
 
+/**
+ * @author Sjaak Kok
+ * @author Rowan Paul Flynn
+ *
+ */
 public class FloorTile extends Tile {
 	/**
-	 * 
+	 * Adds sprite to floortile
 	 * @param sprite image to be drawn as a floor Tile.
 	 */
 	public FloorTile(Sprite sprite) {
